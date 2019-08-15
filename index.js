125dfd33cabfaba44197b083fbd34875b460e39c
Gavin King <gavin.king@acando.no>
2019-08-15 14:44:38 +0200

    handle file uploads

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const formidable = require('formidable');
 const app = express();
 const port = 1234;
 
@@ -8,6 +9,25 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.cookie('name', 'jeff');
   res.json({ message: 'Hello World!' });
+});
+
+app.post('/upload', (req, res) => {
+  var form = new formidable.IncomingForm();
+
+  form.parse(req);
+
+  form.on('fileBegin', (name, file) => {
+    file.path = __dirname + '/uploads/' + file.name;
+  });
+
+  form.on('file', (name, file) => {
+    console.log('Uploaded ' + file.name);
+    res.json({message: 'complete'});
+  });
+
+  form.on('error', (err) => {
+    res.status(500).json({error: err.message});
+  });
 });
 
 app.get('/test-cookie', (req, res) => {
